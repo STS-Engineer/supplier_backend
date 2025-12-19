32f6ac6db03c76612e7caa9497d495377347d273
STS-Engineer <administration.sts@avocarbon.com>
2025-12-19 15:43:30 +0100

    Update supplier.js

--- services/supplier.js
+++ services/supplier.js
@@ -10,8 +10,8 @@ const fs = require('fs');
 // Configure multer for file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-     const uploadDir = path.join(__dirname, '../uploads');
-
+    const uploadDir = path.join(__dirname, './uploads');
+ 
     // Create directory if it doesn't exist
     if (!fs.existsSync(uploadDir)) {
       fs.mkdirSync(uploadDir, { recursive: true });
