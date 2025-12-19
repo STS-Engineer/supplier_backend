4c78d1f0b434d79b838f437e5007d93d818b8fb6
STS-Engineer <administration.sts@avocarbon.com>
2025-12-19 14:04:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 // ✅ Handle preflight requests explicitly
 app.options('*', (req, res) => { 
-  res.header('Access-Control-Allow-Origin', 'https://supplier-mangement.azurewebsites.net');
+  res.header('Access-Control-Allow-Origin', 'https://supplier-mangement.azurewebsites.net');    
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200);
